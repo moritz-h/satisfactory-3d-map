b7a5b84b7f421c1443dad7a6177ef022a9a80e15
Moritz Heinemann <mo@blog-srv.net>
2021-02-16 00:34:56 +0100

    string parsing

--- src/SaveGame/Utils/FileUtils.h
+++ src/SaveGame/Utils/FileUtils.h
@@ -1,7 +1,9 @@
 #ifndef SATISFACTORYSAVEGAME_FILEUTILS_H
 #define SATISFACTORYSAVEGAME_FILEUTILS_H
 
+#include <codecvt>
 #include <istream>
+#include <locale>
 #include <string>
 #include <vector>
 
@@ -26,15 +28,32 @@ namespace SatisfactorySaveGame {
         return vec;
     }
 
+    // https://docs.unrealengine.com/en-US/ProgrammingAndScripting/ProgrammingWithCPP/UnrealArchitecture/StringHandling/CharacterEncoding/index.html
     static std::string read_length_string(std::istream& stream) {
-        auto size = read<uint32_t>(stream);
-        std::string str(size, '\0');
-        stream.read(str.data(), size * sizeof(std::string::value_type));
+        auto size = read<int32_t>(stream);
+
+        if (size == 0) {
+            return std::string();
+        }
+
+        if (size > 0) {
+            std::string str(size, '\0');
+            stream.read(str.data(), size * sizeof(std::string::value_type));
+            if (!stream.good()) {
+                throw std::runtime_error("Error reading from stream!");
+            }
+            str.erase(std::find(str.begin(), str.end(), '\0'), str.end());
+            return str;
+        }
+
+        std::u16string u16str(-size, '\0');
+        stream.read(reinterpret_cast<char*>(u16str.data()), -size * sizeof(std::u16string::value_type));
         if (!stream.good()) {
             throw std::runtime_error("Error reading from stream!");
         }
-        str.erase(std::find(str.begin(), str.end(), '\0'), str.end());
-        return str;
+        u16str.erase(std::find(u16str.begin(), u16str.end(), '\0'), u16str.end());
+        // https://en.cppreference.com/w/cpp/locale/codecvt_utf8_utf16
+        return std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t>{}.to_bytes(u16str);
     }
 } // namespace SatisfactorySaveGame
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,11 @@
 
 #include "SaveGame/SaveGame.h"
 
+#ifdef _WIN32
+#include <Windows.h>
+#include <cstdio>
+#endif
+
 int main(int argc, char* argv[]) {
     std::cout << "[SatisfactorySaveGame]" << std::endl;
     if (argc != 2) {
@@ -10,6 +15,13 @@ int main(int argc, char* argv[]) {
         return 1;
     }
     std::string filename(argv[1]);
+
+#ifdef _WIN32
+    // Set console output to UTF-8
+    // https://stackoverflow.com/a/45622802
+    SetConsoleOutputCP(CP_UTF8);
+    setvbuf(stdout, nullptr, _IOFBF, 1000);
+#endif
 
     try {
         SatisfactorySaveGame::SaveGame s(filename);
