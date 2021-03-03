c32f3c8b6a946d6111e1d38ebffc582244665ae3
Moritz Heinemann <mo@blog-srv.net>
2021-03-03 23:16:34 +0100

    patch zlib with minimal modern cmake file

--- libs/libs.cmake
+++ libs/libs.cmake
@@ -13,37 +13,29 @@ FetchContent_Declare(zlib
   GIT_REPOSITORY https://github.com/madler/zlib.git
   GIT_TAG        v1.2.11)
 FetchContent_GetProperties(zlib)
-if(NOT zlib_POPULATED)
+if (NOT zlib_POPULATED)
   FetchContent_Populate(zlib)
+  file(COPY ${CMAKE_SOURCE_DIR}/libs/zlib/CMakeLists.txt DESTINATION ${zlib_SOURCE_DIR})
   mark_as_advanced(FORCE
     FETCHCONTENT_SOURCE_DIR_ZLIB
-    FETCHCONTENT_UPDATES_DISCONNECTED_ZLIB
-    AMD64
-    ASM686
-    CMAKE_BACKWARDS_COMPATIBILITY
-    EXECUTABLE_OUTPUT_PATH
-    INSTALL_BIN_DIR
-    INSTALL_INC_DIR
-    INSTALL_LIB_DIR
-    INSTALL_MAN_DIR
-    INSTALL_PKGCONFIG_DIR
-    LIBRARY_OUTPUT_PATH)
-  set(CMAKE_PROJECT_INCLUDE_BEFORE "${CMAKE_SOURCE_DIR}/libs/zlib/SetPolicies.cmake")
-  add_subdirectory(${zlib_SOURCE_DIR} ${zlib_BINARY_DIR} EXCLUDE_FROM_ALL)
-  unset(CMAKE_PROJECT_INCLUDE_BEFORE)
-endif()
+    FETCHCONTENT_UPDATES_DISCONNECTED_ZLIB)
+    add_subdirectory(${zlib_SOURCE_DIR} EXCLUDE_FROM_ALL)
+endif ()
 
 # GLFW
 FetchContent_Declare(glfw
   GIT_REPOSITORY https://github.com/glfw/glfw.git
   GIT_TAG        3.3.3)
 FetchContent_GetProperties(glfw)
-if(NOT glfw_POPULATED)
+if (NOT glfw_POPULATED)
   FetchContent_Populate(glfw)
   option(GLFW_BUILD_DOCS "" OFF)
   option(GLFW_BUILD_EXAMPLES "" OFF)
   option(GLFW_BUILD_TESTS "" OFF)
   option(GLFW_INSTALL "" OFF)
+  if (WIN32)
+    option(GLFW_USE_HYBRID_HPG "" ON)
+  endif ()
   mark_as_advanced(FORCE
     FETCHCONTENT_SOURCE_DIR_GLFW
     FETCHCONTENT_UPDATES_DISCONNECTED_GLFW
@@ -52,10 +44,12 @@ if(NOT glfw_POPULATED)
     GLFW_BUILD_EXAMPLES
     GLFW_BUILD_TESTS
     GLFW_INSTALL
+    GLFW_USE_HYBRID_HPG
     GLFW_USE_OSMESA
     GLFW_USE_WAYLAND
     GLFW_VULKAN_STATIC
+    USE_MSVC_RUNTIME_LIBRARY_DLL
     X11_xcb_icccm_INCLUDE_PATH
     X11_xcb_icccm_LIB)
   add_subdirectory(${glfw_SOURCE_DIR} ${glfw_BINARY_DIR} EXCLUDE_FROM_ALL)
-endif()
+endif ()
