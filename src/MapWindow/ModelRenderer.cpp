6de5e8ef518f6ff1260539be624eecc294338516
Moritz Heinemann <mo@blog-srv.net>
2021-04-11 19:30:55 +0200

    additional checks and cleanup

--- src/MapWindow/ModelRenderer.cpp
+++ src/MapWindow/ModelRenderer.cpp
@@ -172,20 +172,28 @@ void Satisfactory3DMap::ModelRenderer::loadSave(const Satisfactory3DMap::SaveGam
 
     for (std::size_t i = 0; i < modelDataList_.size(); i++) {
         auto& modelData = modelDataList_[i];
-        modelData.idBuffer = std::make_unique<glowl::BufferObject>(GL_SHADER_STORAGE_BUFFER, ids[i]);
-        modelData.transformBuffer = std::make_unique<glowl::BufferObject>(GL_SHADER_STORAGE_BUFFER,
-            glm::value_ptr(transformations[i].front()), transformations[i].size() * sizeof(glm::mat4), GL_DYNAMIC_DRAW);
+        modelData.idBuffer.reset();
+        modelData.transformBuffer.reset();
+        if (!ids[i].empty()) {
+            modelData.idBuffer = std::make_unique<glowl::BufferObject>(GL_SHADER_STORAGE_BUFFER, ids[i]);
+            modelData.transformBuffer = std::make_unique<glowl::BufferObject>(GL_SHADER_STORAGE_BUFFER,
+                glm::value_ptr(transformations[i].front()), transformations[i].size() * sizeof(glm::mat4),
+                GL_DYNAMIC_DRAW);
+        }
     }
 
-    splineIds_ = std::make_unique<glowl::BufferObject>(GL_SHADER_STORAGE_BUFFER, splineIds);
-    splineSegments_ = std::make_unique<glowl::BufferObject>(GL_SHADER_STORAGE_BUFFER, splineSegments);
+    splineIds_.reset();
+    splineSegments_.reset();
+    if (numSplineSegments_ > 0) {
+        splineIds_ = std::make_unique<glowl::BufferObject>(GL_SHADER_STORAGE_BUFFER, splineIds);
+        splineSegments_ = std::make_unique<glowl::BufferObject>(GL_SHADER_STORAGE_BUFFER, splineSegments);
+    }
 }
 
 void Satisfactory3DMap::ModelRenderer::render(const glm::mat4& projMx, const glm::mat4& viewMx) {
     shader_->use();
     shader_->setUniform("projMx", projMx);
     shader_->setUniform("viewMx", viewMx);
-    shader_->setUniform("invViewMx", glm::inverse(viewMx));
 
     glActiveTexture(GL_TEXTURE0);
     shader_->setUniform("tex", 0);
@@ -195,28 +203,32 @@ void Satisfactory3DMap::ModelRenderer::render(const glm::mat4& projMx, const glm
             const auto& model = models_[i];
             const auto& modelData = modelDataList_[i];
 
-            shader_->setUniform("modelMx", model.model->modelMx());
-            shader_->setUniform("normalMx", glm::inverseTranspose(glm::mat3(model.model->modelMx())));
-            model.model->bindTexture();
-            modelData.idBuffer->bind(0);
-            modelData.transformBuffer->bind(1);
-            model.model->draw(modelData.numActors);
+            if (modelData.idBuffer != nullptr && modelData.transformBuffer != nullptr) {
+                shader_->setUniform("modelMx", model.model->modelMx());
+                shader_->setUniform("normalMx", glm::inverseTranspose(glm::mat3(model.model->modelMx())));
+                model.model->bindTexture();
+                modelData.idBuffer->bind(0);
+                modelData.transformBuffer->bind(1);
+                model.model->draw(modelData.numActors);
+            }
         }
     }
 
     glBindTexture(GL_TEXTURE_2D, 0);
 
-    splineShader_->use();
-    splineShader_->setUniform("projMx", projMx);
-    splineShader_->setUniform("viewMx", viewMx);
-    splineShader_->setUniform("splineSubdivision", splineSubdivision_);
+    if (numSplineSegments_ > 0 && splineIds_ != nullptr && splineSegments_ != nullptr) {
+        splineShader_->use();
+        splineShader_->setUniform("projMx", projMx);
+        splineShader_->setUniform("viewMx", viewMx);
+        splineShader_->setUniform("splineSubdivision", splineSubdivision_);
 
-    splineIds_->bind(0);
-    splineSegments_->bind(1);
+        splineIds_->bind(0);
+        splineSegments_->bind(1);
 
-    glBindVertexArray(vaEmpty_);
-    glDrawArraysInstanced(GL_POINTS, 0, 1, numSplineSegments_ * splineSubdivision_);
-    glBindVertexArray(0);
+        glBindVertexArray(vaEmpty_);
+        glDrawArraysInstanced(GL_POINTS, 0, 1, numSplineSegments_ * splineSubdivision_);
+        glBindVertexArray(0);
+    }
 
     glUseProgram(0);
 }
