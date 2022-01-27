9b8246833145ead298606d3b7c09f1403642b797
Moritz Heinemann <mo@blog-srv.net>
2022-01-27 01:17:43 +0100

    parse more asset header data

--- src/Pak/AssetFile.cpp
+++ src/Pak/AssetFile.cpp
@@ -114,4 +114,70 @@ Satisfactory3DMap::AssetFile::AssetFile(std::vector<char> uassetData, std::vecto
     int32_t PreloadDependencyOffset = ar.read<int32_t>();
 
     // End FPackageFileSummary header
+
+    // Debug only!
+    if (ar.tell() != NameOffset) {
+        throw std::runtime_error("Unknown format!");
+    }
+
+    for (int32_t i = 0; i < NameCount; i++) {
+        // FNameEntrySerialized
+        std::string name;
+        ar << name;
+        // DummyHashes
+        ar.read<uint16_t>();
+        ar.read<uint16_t>();
+    }
+
+    // Debug only!
+    if (ar.tell() != ImportOffset) {
+        throw std::runtime_error("Unknown format!");
+    }
+
+    // For FName see: https://github.com/gildor2/UEViewer/blob/master/Unreal/UnrealPackage/UnPackage.cpp#L713
+
+    for (int32_t i = 0; i < ImportCount; i++) {
+        // FObjectImport
+        ar.read<int64_t>();                      // FName ClassPackage;
+        ar.read<int64_t>();                      // FName ClassName;
+        int32_t OuterIndex = ar.read<int32_t>(); // FPackageIndex
+        ar.read<int64_t>();                      // FName ObjectName;
+    }
+
+    // Debug only!
+    if (ar.tell() != ExportOffset) {
+        throw std::runtime_error("Unknown format!");
+    }
+
+    for (int32_t i = 0; i < ExportCount; i++) {
+        // FObjectExport
+        ar.read<int32_t>(); // FPackageIndex ClassIndex;
+        ar.read<int32_t>(); // FPackageIndex SuperIndex;
+        ar.read<int32_t>(); // FPackageIndex TemplateIndex;
+        ar.read<int32_t>(); // FPackageIndex OuterIndex;
+        ar.read<int64_t>(); // FName ObjectName;
+        uint32_t Save = ar.read<uint32_t>();
+        int64_t SerialSize = ar.read<int64_t>();
+        int64_t SerialOffset = ar.read<int64_t>();
+        bool bForcedExport = !!ar.read<uint32_t>();
+        bool bNotForClient = !!ar.read<uint32_t>();
+        bool bNotForServer = !!ar.read<uint32_t>();
+        Guid PackageGuid;
+        ar << PackageGuid;
+        uint32_t PackageFlags = ar.read<uint32_t>();
+        bool bNotAlwaysLoadedForEditorGame = !!ar.read<uint32_t>();
+        bool bIsAsset = !!ar.read<uint32_t>();
+        int32_t FirstExportDependency = ar.read<int32_t>();
+        int32_t SerializationBeforeSerializationDependencies = ar.read<int32_t>();
+        int32_t CreateBeforeSerializationDependencies = ar.read<int32_t>();
+        int32_t SerializationBeforeCreateDependencies = ar.read<int32_t>();
+        int32_t CreateBeforeCreateDependencies = ar.read<int32_t>();
+    }
+
+    // Debug only!
+    if (ar.tell() != DependsOffset) {
+        throw std::runtime_error("Unknown format!");
+    }
+
+    // TODO remaining bytes
 }
