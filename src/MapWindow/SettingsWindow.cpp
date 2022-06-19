dd989f0b8bdd0958b17a8e46bfad3b608a5fbbd1
Moritz Heinemann <mo@blog-srv.net>
2022-06-19 22:09:50 +0200

    table layout for settings window

--- src/MapWindow/SettingsWindow.cpp
+++ src/MapWindow/SettingsWindow.cpp
@@ -13,17 +13,31 @@ void Satisfactory3DMap::SettingsWindow::renderGui() {
         return;
     }
 
-    ImGui::SetNextWindowSize(ImVec2(600.0f, 400.0f), ImGuiCond_Appearing);
+    ImGui::SetNextWindowSize(ImVec2(800.0f, 600.0f), ImGuiCond_Appearing);
     const auto& size = ImGui::GetIO().DisplaySize;
     ImGui::SetNextWindowPos(ImVec2(size.x * 0.5f, size.y * 0.5f), ImGuiCond_Appearing, ImVec2(0.5f, 0.5f));
     ImGui::Begin("Settings##SettingsWindow", &show_);
-    if (ImGui::Button("Select")) {
-        auto dir = selectFolder("Select Satisfactory directory ...");
-        if (dir.has_value()) {
-            config_->setGameDirectory(dir.value());
+    if (ImGui::BeginTable("tableSettings", 2, ImGuiTableFlags_Borders | ImGuiTableFlags_SizingFixedFit)) {
+        ImGui::TableSetupColumn("Setting");
+        ImGui::TableSetupColumn("Value", ImGuiTableColumnFlags_WidthStretch);
+        ImGui::TableHeadersRow();
+
+        // Game directory
+        ImGui::TableNextRow();
+        ImGui::TableNextColumn();
+        ImGui::Text("Game Directory");
+        ImGui::TableNextColumn();
+        if (ImGui::Button("Select")) {
+            auto dir = selectFolder("Select Satisfactory directory ...");
+            if (dir.has_value()) {
+                config_->setGameDirectory(dir.value());
+            }
         }
+        ImGui::SameLine();
+        ImGui::Text("\"%s\"", config_->getGameDirectory().string().c_str());
+        ImGui::Text("(change requires restart)");
+
+        ImGui::EndTable();
     }
-    ImGui::SameLine();
-    ImGui::Text("Game Directory: \"%s\" (change requires restart)", config_->getGameDirectory().string().c_str());
     ImGui::End();
 }
