4974d0225021fabcf06fab0c123d5ca9a253eca8
Moritz Heinemann <mo@blog-srv.net>
2023-01-17 00:38:40 +0100

    simplify array serialization

--- src/GameTypes/Arrays/EnumArray.cpp
+++ src/GameTypes/Arrays/EnumArray.cpp
@@ -3,15 +3,7 @@
 #include "ArrayVisitor.h"
 
 void Satisfactory3DMap::EnumArray::serialize(Archive& ar) {
-    int32_t count = static_cast<int32_t>(array_.size());
-    ar << count;
-    if (ar.isIArchive()) {
-        array_.resize(count);
-    }
-
-    for (auto& e : array_) {
-        ar << e;
-    }
+    ar << array_;
 }
 
 void Satisfactory3DMap::EnumArray::accept(Satisfactory3DMap::ArrayVisitor& v) {
--- src/GameTypes/Arrays/ObjectArray.cpp
+++ src/GameTypes/Arrays/ObjectArray.cpp
@@ -3,15 +3,7 @@
 #include "ArrayVisitor.h"
 
 void Satisfactory3DMap::ObjectArray::serialize(Archive& ar) {
-    int32_t count = static_cast<int32_t>(array_.size());
-    ar << count;
-    if (ar.isIArchive()) {
-        array_.resize(count);
-    }
-
-    for (auto& objRef : array_) {
-        ar << objRef;
-    }
+    ar << array_;
 }
 
 void Satisfactory3DMap::ObjectArray::accept(Satisfactory3DMap::ArrayVisitor& v) {
