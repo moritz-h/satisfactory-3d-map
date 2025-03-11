c825ca7931e9f7988ed1cac2a34a59e34bc73ad7
Moritz Heinemann <mo@blog-srv.net>
2025-03-12 00:55:40 +0100

    cleanup cmake

--- cmake/warning_level.cmake
+++ cmake/warning_level.cmake
@@ -3,9 +3,7 @@
 function(set_warning_level target)
   if (SATISFACTORY3DMAP_STRICT_WARNINGS)
     if (MSVC)
-      # MSVC has an experimental feature to disable warnings from external libs, see https://devblogs.microsoft.com/cppblog/broken-warnings-theory/.
-      # Maybe in future there is CMake support for this feature, see https://gitlab.kitware.com/cmake/cmake/-/issues/17904.
-      target_compile_options(${target} PRIVATE /W4 /WX /external:anglebrackets /external:W3)
+      target_compile_options(${target} PRIVATE /W4 /WX)
       target_compile_definitions(${target} PRIVATE
         _CRT_SECURE_NO_WARNINGS
         _SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING)
